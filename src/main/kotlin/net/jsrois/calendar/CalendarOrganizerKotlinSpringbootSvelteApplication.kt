99a8a76b59ced45467afcf62ec76ce59ad5556ba
Javi <javi@jsrois.net>
2023-06-23 15:10:15 +0200

    Initial commit

---
+++ src/main/kotlin/net/jsrois/calendar/CalendarOrganizerKotlinSpringbootSvelteApplication.kt
@@ -0,0 +1,11 @@
+package net.jsrois.calendar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalendarOrganizerKotlinSpringbootSvelteApplication
+
+fun main(args: Array<String>) {
+    runApplication<CalendarOrganizerKotlinSpringbootSvelteApplication>(*args)
+}
---
+++ src/test/kotlin/net/jsrois/calendar/CalendarOrganizerKotlinSpringbootSvelteApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.jsrois.calendar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CalendarOrganizerKotlinSpringbootSvelteApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
